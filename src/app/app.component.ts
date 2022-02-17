5f312bd6916a06e2d7ab1d8ff9999ce04e4ef85c
rvpatil1432 <rvpatil1432@gmail.com>
2022-02-17 08:52:29 +0530

    Added:Use case to launch site when user click on logo.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Helloworld';
   imgUrl = "../assets/Bridgelabz.png";
+  url = "https://www.bridgelabz.com/";
+
   ngOnInit():void{
     this.title = "Hello From Bridgelabz.";
   }
+
+  onClick($event:any){
+    console.log("Save button is clicked."+$event);
+    window.open(this.url,"_blank");
+
+  }
+
 }
