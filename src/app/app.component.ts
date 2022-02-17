5bb61b276fa64369622c18f6ff972ed5d8b1bd14
rvpatil1432 <rvpatil1432@gmail.com>
2022-02-17 10:31:29 +0530

    Added:Use case to add validation on username input to enter valid name.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,28 @@ export class AppComponent {
   title = 'Helloworld';
   imgUrl = "../assets/Bridgelabz.png";
   url = "https://www.bridgelabz.com/";
-  userName:String = "";
+  userName: string = "";
+  nameError: string = "";
 
-  ngOnInit():void{
+
+  ngOnInit(): void {
     this.title = "Hello From Bridgelabz.";
   }
 
-  onClick($event:any){
-    console.log("Save button is clicked."+$event);
-    window.open(this.url,"_blank");
+  onClick($event: any) {
+    console.log("Save button is clicked." + $event);
+    window.open(this.url, "_blank");
 
+  }
+
+  onInput($event: any) {
+    console.log("Change event occured!",$event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Incorrect.";
   }
 
 }
